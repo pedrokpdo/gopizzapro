34f9b8cdcaecfde8b0d06925db4f81acb9ef2e23
pedrokpdo <pedromotoehm@gmail.com>
2022-05-16 17:35:59 -0300

    status bar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { useFonts, DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 import { ThemeProvider } from 'styled-components/native'
 import theme from './src/theme'
@@ -11,6 +12,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar style='light' translucent backgroundColor='transparent'/>
       <SignIn />
     </ThemeProvider>
 
