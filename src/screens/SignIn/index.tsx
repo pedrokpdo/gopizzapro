c73871d3926fcd85317e77967aac793a1f3c8e8f
pedrokpdo <pedromotoehm@gmail.com>
2022-05-16 17:59:58 -0300

    componente imput

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { Text } from "react-native";
 import { Container } from "./styles";
+import { Input } from '../../components/Input'
 export function SignIn() {
     return (
         <Container>
-            
+            <Input
+                placeholder="E-Mail"
+                type="secondary"
+                autoCorrect={false}
+                autoCapitalize='none'
+            />
+            <Input
+                placeholder="Senha"
+                type="secondary"
+                secureTextEntry
+            />
         </Container>
     )
 }
