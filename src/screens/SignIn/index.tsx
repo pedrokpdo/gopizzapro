940a392a9337f6127a600f1c6960314a958236f1
pedrokpdo <pedromotoehm@gmail.com>
2022-05-17 09:08:04 -0300

    finalizando signIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,23 +1,34 @@
 import React from "react";
 import { Text } from "react-native";
-import { Container } from "./styles";
+import * as C from "./styles";
+import brandImg from '../../assets/brand.png'
 import { Input } from '../../components/Input'
+import { KeyboardAvoidingView, Platform } from 'react-native'
 import { Button } from "../../components/Button";
 export function SignIn() {
     return (
-        <Container>
-            <Input
-                placeholder="E-Mail"
-                type="secondary"
-                autoCorrect={false}
-                autoCapitalize='none'
-            />
-            <Input
-                placeholder="Senha"
-                type="secondary"
-                secureTextEntry
-            />
-            <Button title='Entrar' type="secondary"/>
-        </Container>
+        <C.Container>
+            <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+                <C.Content>
+                    <C.Brand source={brandImg}/>
+                    <C.Title>Login</C.Title>
+                    <Input
+                        placeholder="E-Mail"
+                        type="secondary"
+                        autoCorrect={false}
+                        autoCapitalize='none'
+                    />
+                    <Input
+                        placeholder="Senha"
+                        type="secondary"
+                        secureTextEntry
+                    />
+                    <C.ForgotPasswordButton>
+                        <C.ForgotPasswordLabel>Esqueci minha senha</C.ForgotPasswordLabel>
+                    </C.ForgotPasswordButton>
+                    <Button title='Entrar' type="secondary" />
+                </C.Content>
+            </KeyboardAvoidingView>
+        </C.Container>
     )
 }
